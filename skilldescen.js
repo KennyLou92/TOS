af75486a7fb6261811b81a97e8e7253ae07877af
KennyLou92 <114062621+KennyLou92@users.noreply.github.com>
2025-04-17 09:13:53 +0800

    Update skilldescen.js

--- skilldescen.js
+++ skilldescen.js
@@ -1,4 +1,4 @@
-const skilldescen = {
+const skilldesc = {
   "DESC": { name: "I. Tap 1 Runestone:\n\u21d2 Modify that column to\n    become Protected Runestones.\nII. If the column below a \n   \u3010Classical Epic\u3011 Member is tapped:\n\u21d2 That Member changes to the \n    corresponding \u3010Ensemble\u3011mode\n    according to the Attribute tapped.", description: "" },
   "3261": { name: "Swastika of Light", description: "I. Turn all Runestones into Enchanted Light and Enchanted Heart Runestones of [board=AAEBAAYFC/nkz7wCDYabsMMBAA==]fixed numbers and fixed positions[/board].\nFor 1 Round,\nII. Light and Dragon Attack x 2." },
   "0": { name: "Ceaseless Slaughter", description: "Nil" },
